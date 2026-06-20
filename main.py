ffba1a70c9503f104cb8a6fa8e1dc43a3b6e8bf5
proxyrot <coffinatyourdoor@gmail.com>
2026-06-20 11:39:27 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,258 @@
+import time
+import os
+import drink3
+
+# for drink imports
+third_drink = False
+keep_asking = True
+name = ""
+BOLD = '\033[1m'
+ITALIC = '\033[3m'
+BOLD_ITALIC = '\033[1;3m'
+RESET = ' \033[0m'
+#clear os func
+def clear_os():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def start():
+    print("clack...")
+    time.sleep(0.5)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    time.sleep(0.5)
+    print("clack...")
+    time.sleep(1)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    time.sleep(1)
+    print("clack...")
+    time.sleep(1)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    time.sleep(3)
+    print("You hear that?...")
+    time.sleep(2.5)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    time.sleep(1.5)
+    print("THAT'S THE SOUND OF YOUR FREEDOM!!")
+    time.sleep(3)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(" Seems like you’ve finally escaped what they call “parents” and “school” (I think that’s what they’re called?..)")
+    time.sleep(4)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Don’t get it wrong, you’re an adult, but stupidly persuaded by the thought of NO RENT and NO FOOD COSTS, you decided to stay with your parents…. Even AFTER you graduated high school three years ago.. (Quite brainless of you, I know)")
+    time.sleep(10)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("There you sat day after day: work, eat, and work some darn more.")
+    time.sleep(6)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("You occasionally talked to your friends, venting to them about the horrors of living with barn animals that just happen to be of the same blood as you, but, of course they would just say something along the lines of, “ We told you (insert name)")
+    time.sleep(10)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"{BOLD}Traceback (most recent call last):{RESET}")
+    time.sleep(0.3)
+    print('  File "auth_protocol.py", line 65, in start')
+    time.sleep(0.3)
+    print('    data_stream = self.verify_input(name)')
+    print('  File "name_check.py", line 11, in verify_input')
+    time.sleep(0.3)
+    print(f'    raise ProtocolError: Unrecognized format or excessive length for string "name".'"")
+    print(f"{BOLD}ProtocolError: RESOURCE_LIMIT_EXCEEDED: Input exceeds allocated storage buffer (MAX_LEN: 16 characters).{RESET}")
+    time.sleep(10)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    time.sleep(3)
+    print(f"{BOLD}Traceback (most recent call last):{RESET}")
+    time.sleep(0.3)
+    print('  File "auth_protocol.py", line 65, in start')
+    print('    data_stream = self.verify_input(name)')
+    print('  File "name_check.py", line 11, in verify_input')
+    time.sleep(0.3)
+    print(f'    raise ProtocolError: Unrecognized format or excessive length for string "name".')
+    print(f"{BOLD}ProtocolError: RESOURCE_LIMIT_EXCEEDED: Input exceeds allocated storage buffer (MAX_LEN: 16 characters).{RESET}")
+    time.sleep(6)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    time.sleep(3)
+    name = input("wait that doesn’t look right.. What’s your name again? : ")
+    time.sleep(1.5)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("ahem.. back to what I was saying...")
+    time.sleep(4)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("We told you to,"+name+" move into the campus tard lol.. Now you reap what you sow.” right before they would vanish to another frat party.")
+    time.sleep(5)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Given the situation and the stress stacking up like a poorly built Jenga tower, you finally decided enough was ENOUGH.")
+    time.sleep(3)
+    clear_os()
+    print("You were sick of being overworked, sick of scrubbing dishes like a sentient sponge, and very sick of pretending the food you made was anything other than edible disappointment.")
+    time.sleep(3)
+    clear_os()
+    print(" You had enough side-job money to rent a tiny house or at least a place that technically had walls, so the thought was there")
+    time.sleep(2)
+    clear_os()
+    print("Running on fury, caffeine, and the kind of confidence that only comes from being absolutely done, you marched down the hallway and slammed your parents’ door open, ready to launch into the speech of your life.")
+    time.sleep(5)
+    clear_os()
+    time.sleep(3)
+    print("Then you made eye contact with your parents.")
+    time.sleep(3)
+    clear_os()
+    time.sleep(1)
+    print("And suddenly every brave thought in your brain just… evaporated. Like steam from the dishwater you were tragically familiar with")
+    time.sleep(2)
+    clear_os()
+    print("You shut the door again in slow motion, pretending that it was totally intentional, and backed away before they could ask what your problem was.")
+    time.sleep(3)
+    clear_os()
+    print(f"{BOLD}Clearly you needed a plan.{RESET}")
+    time.sleep(3)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1.5)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Preferably one that didn’t end with you getting emotionally folded like laundry.")
+    time.sleep(2)
+    clear_os()
+    print("Welcome aboard the ship " +name+ "! \nYou are free to explore around, just don't go to the backdoor past the bridge.")
+    time.sleep(3)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1.5)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Follow me and we can go to the main drinking area *wink* *wink* ")
+    time.sleep(1.5)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("footsteps echoing...")
+    time.sleep(1)
+    print("footsteps echoing...")
+    time.sleep(1)
+    print("footsteps echoing...")
+    time.sleep(1.5)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("We're here!") 
+
+
+def drink_selection():
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Alright! Let's get your drink started")
+        time.sleep(1)
+        print(".")
+        time.sleep(1)
+        print(".")
+        time.sleep(1)
+        print(".")
+        time.sleep(1.5)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        print("You take a glance at the menu and decide to go with the...?")
+        drink = input("Pick your drink...\n Type 1 for Seasick Slinger \n Type 2 for Captain’s Mistake \n Type 3 for Tropical Overcompensation \n Type 4 for The Kraken’s Tears \n Type 5 for The Siren's Song \n")
+
+        if drink == "1":
+            time.sleep(1.5)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("You chose Seasick Slinger. Bold choice!")
+            break
+        elif drink == "2":
+            time.sleep(1.5)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Ah, Captain’s Mistake! Just like this code was before the danm writer fixed it.")
+            time.sleep(1)
+            print("Just kidding lol..")
+            time.sleep(1)
+            print("heh..")
+            time.sleep(0.5)
+            print("uhm anyways.")
+            break
+        elif drink == "3":
+            time.sleep(1.5)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Tropical Overcompensation? Overcompensating for something, are we?")
+            drink3.drink3()
+            break
+        elif drink == "4":
+            time.sleep(1.5)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("The Kraken’s Tears. That’s just Kraken-flavored sadness.")
+            break
+        elif drink == "5":
+            time.sleep(1.5)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("The Siren's Song? Don't blame me if you get lured into bad decisions! \nAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA!!")
+            time.sleep(5)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("excuse me for my uh... humor! heh...")
+            break
+        else:
+            time.sleep(1.5)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("The waiter gives you a concerened look, \"Hmm, that's not on the menu. Try again, it's not rocket science..\"")
+
+# ------------------ START THE PROGRAM --------------------
+
+start()
+
+# DRINK LOGIC
+while keep_asking:
+    time.sleep(2)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    user_drink = input("Wanna drink? (type yes or no): ").lower()
+
+    if user_drink == "yes":
+        drink_selection()
+        break
+
+    elif user_drink == "no":
+        time.sleep(1.5)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Tch whatever man, I guess you're no fun to have around.")
+        time.sleep(2)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        try_again = input("You sure? You're gonna miss out.. (type yes or no): ").lower()
+
+        if try_again == "yes":
+            print(".")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Okay, fine. Have fun with your distasteful saliva.")
+            break
+        elif try_again == "no":
+            drink_selection()  # <- LOOP BACK TO DRINK OPTIONS
+            break
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("So I see this guy decided to be stupid! HAHAHAHAH!")
+            time.sleep(3)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Let’s just assume you meant 'no'.")
+            break
+    else:
+        print("You decided to enter something else. Like, it wasn't that hard. The options were literally 'yes' or 'no'.")
+        time.sleep(2)
+        continue
+
