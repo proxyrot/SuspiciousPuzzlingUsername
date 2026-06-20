c74160a6900e5970432322e7207f6fd45627f17a
proxyrot <coffinatyourdoor@gmail.com>
2026-06-20 11:40:49 -0500

    fightgame1.py

---
+++ fightgame1.py
@@ -0,0 +1,22 @@
+import time
+import os 
+from main import name
+
+#-------------------------------------------
+
+
+class enemy:
+  def __init__(self,name,health,damage,buffs):
+    self.name = name
+    self.health = health
+    self.damage = damage
+    self.buffs = []
+    self.buffs.append(buffs)
+
+class you:
+  def __init__(self,,health,damage,buffs):
+     self.name = name
+     self.health = health
+     self.damage = damage
+     self.buffs = []
+    
